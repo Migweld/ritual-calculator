7c0534e4007a1427542751236a3f66a895dfc187
Craig Thompson <craig@flodesign.co.uk>
2018-05-03 17:54:21 +0100

    Added controls to select realm and ritual

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './wiz.png';
 import './App.css';
+import rituals from './rituals';
 
 class App extends Component {
+  state = {
+    realm: null,
+  };
+
+  changeRealm = realm => {
+    console.log(realm.target.name);
+    this.setState({
+      realm: realm.target.name,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ritual Calculator</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h2>Choose a realm</h2>
+          {Object.keys(rituals).map(realm => (
+            <button onClick={this.changeRealm} name={realm}>
+              {realm.charAt(0).toUpperCase() + realm.slice(1)}
+            </button>
+          ))}
         </p>
+        {this.state.realm !== null ? (
+          <div>
+            <h2>
+              Choose a {this.state.realm.charAt(0).toUpperCase() + this.state.realm.slice(1)} ritual
+            </h2>
+            <select>
+              {rituals[this.state.realm].map(ritual => <option>{ritual.Name}</option>)}
+            </select>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
